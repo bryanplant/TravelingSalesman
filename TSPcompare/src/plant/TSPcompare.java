c461ff967a0eb9dfa95a9c7be7596883bd03a5d8
Bryan Plant <bryanplant@gmail.com>
2017-04-03 09:15:51 -0600

    First Commit

---
+++ TSPcompare/src/plant/TSPcompare.java
@@ -0,0 +1,11 @@
+package plant;
+
+public class TSPcompare {
+	public static void main(String[] args){
+		TSPcompare tsp = new TSPcompare(Integer.parseInt(args[0]));
+	}
+	
+	public TSPcompare(int v){
+		System.out.println(v);
+	}
+}
