dc8dbf393d89d878d3a7be639aeccaae0d9a72d1
Bryan Plant <bryanplant@gmail.com>
2017-04-03 17:33:41 -0600

    Greedy alg

--- src/plant/TSPcompare.java
+++ src/plant/TSPcompare.java
@@ -1,11 +1,41 @@
 package plant;
 
+import java.util.Random;
+import java.math.*;
+
 public class TSPcompare {
-	public static void main(String[] args){
-		TSPcompare tsp = new TSPcompare(Integer.parseInt(args[0]));
+	
+	Random rand = new Random();
+	double[][] v;
+	
+	public TSPcompare(int vNum){
+		v = new double[vNum][2];
+		
+		for(int i = 0; i < vNum; i++){
+			v[i][0] = rand.nextDouble()*100;
+			v[i][1] = rand.nextDouble()*100;
+		}
+		
+		/*for(int i = 0; i < v; i++){
+			System.out.println("Vert" + i + ": " + vert[i][0] + ", " + vert[i][1]);
+		}*/
 	}
 	
-	public TSPcompare(int v){
-		System.out.println(v);
+	public void greedy(){
+		boolean[] visited = new boolean[v.length];
+		
+		for(int i = 0; i < v.length; i++){
+			int near = 0;
+			double nearValue = 100;
+			
+			for(int j = 0; j < v.length; j++){
+				//double value = 
+			}
+		}
+	}
+	
+	public static void main(String[] args){
+		TSPcompare tsp = new TSPcompare(Integer.parseInt(args[0]));
+		tsp.greedy();
 	}
 }
